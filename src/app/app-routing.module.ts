66c9ae67b531b7432f028723583dd40e3b929dcf
jesus bohorquez <jesuspabon791@gmail.com>
2024-10-17 19:11:17 -0500

    refactoring part one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { CategoryComponent } from './components/pages/category/category.component';
 import { BrandComponent } from './components/pages/brand/brand.component';
 import { ArticleComponent } from './components/pages/article/article.component';
+import { AuxBodegaComponent } from './components/pages/aux-bodega/aux-bodega.component';
 import { APP_CONSTANTS } from 'src/styles/constants';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: APP_CONSTANTS.CATEGORIES, component: CategoryComponent },
   { path: APP_CONSTANTS.BRANDS, component: BrandComponent },
   { path: APP_CONSTANTS.ARTICLES, component: ArticleComponent },
+  { path: APP_CONSTANTS.AUXBODEGA, component:  AuxBodegaComponent},
   { path: APP_CONSTANTS.DEFAULT, redirectTo: `/${APP_CONSTANTS.HOME}`, pathMatch: 'full' },
   { path: APP_CONSTANTS.WILDCARD, redirectTo: `/${APP_CONSTANTS.HOME}`, pathMatch: 'full' }
 ];
